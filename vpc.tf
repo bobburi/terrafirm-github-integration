d8cd1acc0750ce206f3b799077dc975c47b3e434
bobburi <bobburirajesh006@gmail.com>
2024-10-01 12:34:21 +0530

    added new files

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,5 @@
 resource "google_compute_network" "vpc_123" {
-  project                 = "test1"
+  project                 = "test123-436512"
   name                    = "vpc-11"
   auto_create_subnetworks = true
   mtu                     = 1460
